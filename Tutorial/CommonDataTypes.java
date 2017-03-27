fd2ed07fc7e51873a3cf8ee78fde32d7d86f2192
Roop Kumar Sriramulu <roopkumar_d19@hotmail.com>
2017-03-26 21:47:33 -0700

    Update CommonDataTypes.java

--- Tutorial/CommonDataTypes.java
+++ Tutorial/CommonDataTypes.java
@@ -8,11 +8,19 @@ public class Application {
 		double myDouble = 23.4534;
 		float myFloat = 234.34f; //shorter version of double.Pls note the "f" at the end of the value declared.
 		
+		char myChar = 'A';
+		boolean myBool = true;
+		
+		byte myByte = 127; //this data type can hold a maximum of 128 bytes
+		
 		System.out.println(myNumber);
 		System.out.println(myShort);
 		System.out.println(myLong);
 		System.out.println(myDouble);
 		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBool);
+		System.out.println(myByte);
 	}
 
 }
